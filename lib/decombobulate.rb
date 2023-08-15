2f85ee8fcf14c8fc018b11750126edcd40499fe8
Christopher Guess <cguess@gmail.com>
2023-08-15 15:32:29 -0400

    add more tests and refactor the row parsing to be more robust and simpler

--- lib/decombobulate.rb
+++ lib/decombobulate.rb
@@ -3,6 +3,7 @@
 require_relative "decombobulate/version"
 require "JSON"
 require "CSV"
+require "debug"
 
 class Decombobulate
   class Error < StandardError; end
@@ -12,48 +13,92 @@ class Decombobulate
   def initialize(json)
     # Parse the JSON if we need to
     json = Json.parse(json) if json.is_a?(String)
+
+
+    # Yes, I tried a case/when statement but for some reason that doesn't work when looking at class types
     if json.is_a?(Array)
       @headers = parse_headers_from_json(json.first)
       @rows = json.map do |json_row|
-        parse_json_object_into_row(json_row)
+        collapse_json_object_row_to_csv_row(json_row)
       end
-    else
+    elsif json.is_a?(Hash)
       @headers = parse_headers_from_json(json)
-      @rows = [parse_json_object_into_row(json)]
+      @rows = [collapse_json_object_row_to_csv_row(json)]
+    else
+      raise "JSON object must have an array or object as the top level object."
     end
   end
 
-  def to_csv
-    CSV.generate do |csv|
-      csv << @headers
-      @rows.each do |row|
-        csv << row
+  def to_csv(file_name = nil)
+    if file_name.nil?
+      csv_final = CSV.generate(encoding: "UTF-8") do |csv|
+        csv << @headers
+        @rows.each do |row|
+          csv << row
+        end
       end
+    else
+      CSV.open(file_name, "wb", encoding: "UTF-8") do |csv|
+        csv << @headers
+        @rows.each do |row|
+          csv << row
+        end
+      end
+
+      return true
     end
+
+    csv_final
   end
 
   def parse_headers_from_json(json, parent_key = nil)
     @headers ||= []
-    json.keys.each do |key|
-      flattened_key = parent_key.nil? ? key.to_s : "#{parent_key}.#{key}"
 
-      unless json[key].is_a?(Hash)
-        @headers << flattened_key
-      else
-        parse_headers_from_json(json[key], flattened_key)
+    if json.is_a?(Hash)
+      json.keys.each do |key|
+        flattened_key = parent_key.nil? ? key.to_s : "#{parent_key}.#{key}"
+
+        if json[key].is_a?(Hash) || json[key].is_a?(Array)
+          parse_headers_from_json(json[key], flattened_key)
+        else
+          @headers << flattened_key
+        end
       end
+    elsif json.is_a?(Array)
+      json.each do |j_object|
+        parse_headers_from_json(j_object, parent_key)
+      end
+    else
+      # This mean's we either have a parsing error or we're in an array.
+      # I'm assuming the JSON coming in is valid, so we're going to just skip this
+      return nil
     end
 
     @headers
   end
 
-  def parse_json_object_into_row(json)
-    @headers.map do |header|
-      header_array = header.split(".").map(&:to_sym)
-      value = json.dig(*header_array)
-      value = value.join(", ") if value.is_a?(Array)
+  def collapse_json_object_row_to_csv_row(json)
+    # Time to do a graph traversal! And recursively! Java 103 is usefuL!!!!
+    row_array = []
 
-      value
+    if json.is_a?(Array)
+      json.each do |j_object|
+        if j_object.is_a?(Array) || j_object.is_a?(Hash)
+          row_array << collapse_json_object_row_to_csv_row(j_object)
+        else
+          row_array << j_object
+        end
+      end
+    elsif json.is_a?(Hash)
+      json.keys.each do |key|
+        if json[key].is_a?(Array) || json[key].is_a?(Hash)
+          row_array << collapse_json_object_row_to_csv_row(json[key])
+        else
+          row_array << json[key]
+        end
+      end
     end
+
+    row_array.flatten
   end
 end
--- test/test_decombobulate.rb
+++ test/test_decombobulate.rb
@@ -55,7 +55,7 @@ class TestDecombobulate < Minitest::Test
     }
 
     decombobulate_object = Decombobulate.new(object)
-    assert_equal ["test_1", "test_2"], decombobulate_object.parse_json_object_into_row(object)
+    assert_equal ["test_1", "test_2"], decombobulate_object.collapse_json_object_row_to_csv_row(object)
   end
 
   def test_a_one_level_nested_structure_row
@@ -68,7 +68,7 @@ class TestDecombobulate < Minitest::Test
     }
 
     decombobulate_object = Decombobulate.new(object)
-    assert_equal ["test_1", "test_2", "test_3"], decombobulate_object.parse_json_object_into_row(object)
+    assert_equal ["test_1", "test_2", "test_3"], decombobulate_object.collapse_json_object_row_to_csv_row(object)
   end
 
   def test_a_two_level_nested_structure_row
@@ -85,7 +85,7 @@ class TestDecombobulate < Minitest::Test
     }
 
     decombobulate_object = Decombobulate.new(object)
-    assert_equal ["test_1", "test_2", "test_3", "test_4", "test_5"], decombobulate_object.parse_json_object_into_row(object)
+    assert_equal ["test_1", "test_2", "test_3", "test_4", "test_5"], decombobulate_object.collapse_json_object_row_to_csv_row(object)
   end
 
   def test_an_array_is_converted_to_a_string
@@ -95,7 +95,7 @@ class TestDecombobulate < Minitest::Test
     }
 
     decombobulate_object = Decombobulate.new(object)
-    assert_equal [["test_1", "test_2, test_3, test_4"]], decombobulate_object.rows
+    assert_equal [["test_1", "test_2", "test_3", "test_4"]], decombobulate_object.rows
   end
 
   def test_an_array_of_top_level_objects_works
@@ -105,7 +105,17 @@ class TestDecombobulate < Minitest::Test
     }]
 
     decombobulate_object = Decombobulate.new(object)
-    assert_equal [["test_1", "test_2, test_3, test_4"]], decombobulate_object.rows
+    assert_equal [["test_1", "test_2", "test_3", "test_4"]], decombobulate_object.rows
+  end
+
+  def test_a_nested_array_of_objects_works
+    object = [{
+      a: "test_1",
+      b: [{ c: "test_2", d: "test_3", e: "test_4" }]
+    }]
+
+    decombobulate_object = Decombobulate.new(object)
+    assert_equal [["test_1", "test_2", "test_3", "test_4"]], decombobulate_object.rows
   end
 
   def test_can_generate_csv
